6bfb77c0b84e1508568fa801f863ded79d0a20a3
aidansedgewick <aidansedgewick203@gmail.com>
2023-06-01 14:26:13 +0200

    add some code

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='dk154_targets',
+    version='0.1.0',
+    description='dk154_targets',
+    author='Aidan S',
+    author_email='aidan.sedgewick@gmail.com',
+    #url='',
+    packages=['dk154_targets',],
+)
