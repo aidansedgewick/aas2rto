cf95b3b2c0bb77acfb916ca69183e2ead1022718
aidansedgewick <aidansedgewick203@gmail.com>
2023-06-09 09:35:47 +0200

    scratch

--- dk154_targets/paths.py
+++ dk154_targets/paths.py
@@ -10,7 +10,10 @@ default_outputs_path = base_path / "outputs"
 test_path = base_path / "test_dk154_targets"
 test_data_path = test_path / "test_data"
 
+scratch_path = base_path / "scratch"
+
 
 def build_paths():
     config_path.mkdir(exist_ok=True, parents=True)
     test_data_path.mkdir(exist_ok=True, parents=True)
+    scratch_path.mkdir(exist_ok=True, parents=True)
