ff60b2849d8b277226b334799dc778b6f3675b73
aidansedgewick <aidansedgewick203@gmail.com>
2023-07-05 15:50:23 +0200

    test remove yaml from __init__

--- dk154_targets/__init__.py
+++ dk154_targets/__init__.py
@@ -1,13 +1,14 @@
 import logging.config
-import yaml
 
-from .target import Target, TargetData
-from .target_selector import TargetSelector
+# import yaml
 
-from . import paths
+from dk154_targets.target import Target, TargetData
+from dk154_targets.target_selector import TargetSelector
 
-default_logging_config = paths.config_path / "logging/default_logging.yaml"
-if default_logging_config.exists():
-    with open(default_logging_config, "rt") as f:
-        log_config = yaml.safe_load(f.read())
-    logging.config.dictConfig(log_config)
+from dk154_targets import paths
+
+# default_logging_config = paths.config_path / "logging/default_logging.yaml"
+# if default_logging_config.exists():
+#     with open(default_logging_config, "rt") as f:
+#         log_config = yaml.safe_load(f.read())
+#     logging.config.dictConfig(log_config)
