fdb966c592767e3569e3108be2679b3624a0f81a
aidansedgewick <aidansedgewick203@gmail.com>
2023-10-17 14:55:45 +0200

    bug in modelling __init__ py

--- dk154_targets/modeling/__init__.py
+++ dk154_targets/modeling/__init__.py
@@ -1,9 +1,3 @@
-from dk154_targets.modeling.example_functions import (
-    empty_modeling,
-)
+from dk154_targets.modeling.example_functions import empty_modeling
 
-from dk154_targets.modeling.sncosmo import (
-    sncosmo_salt,
-    sncosmo_model,
-    sncosmo_model_emcee,
-)
+from dk154_targets.modeling.sncosmo import sncosmo_salt
