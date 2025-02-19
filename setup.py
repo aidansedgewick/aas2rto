08c7fc6a5f2953b002a0dd60218908669d75dfbc
aidansedgewick <aidansedgewick203@gmail.com>
2025-02-19 11:06:24 +0100

    incr minor version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aas2rto",
-    version="0.1.0",
+    version="0.2.0",
     description="aas2rto",
     author="Aidan S",
     author_email="aidan.sedgewick@gmail.com",
